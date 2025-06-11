50409de5857f7678e6348289a0f451940e5455d5
Sven Meyer <sven.meyer@ns.nl>
2025-06-11 15:28:10 +0200

    Add test: the board starts empty

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,11 +1,14 @@
-import { sum } from '../src/main';
+import { TicTacToeGame } from '../src/main';
 
-describe('Sum should be', () => {
-  test('total of 3 when adding 1 and 2', () => {
-    const expectedOutput = 3;
-
-    let result = sum(1, 2);
-
-    expect(result).toBe(expectedOutput);
+describe('Starting a new game', () => {
+  test('The board starts empty', () => {
+    expect(new TicTacToeGame().board).toBe(`
+     | | 
+    -+-+-
+     | | 
+    -+-+-
+     | | 
+    `);
   });
+
 });
