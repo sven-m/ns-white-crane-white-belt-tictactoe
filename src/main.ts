6d93067c93bff48e190da9564e3d4abb77aebf9e
Sven Meyer <sven.meyer@ns.nl>
2025-06-11 15:55:50 +0200

    Satisfy test: First player is x

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,15 @@
+export type Player = 'x';
+
 export class TicTacToeGame {
+  private _currentPlayer: Player
+
+  constructor() {
+    this._currentPlayer = 'x'
+  }
+
+  get currentPlayer(): Player {
+    return this._currentPlayer
+  }
 
   get board(): string {
     return `
