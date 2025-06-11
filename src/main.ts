4df8fb66a40bdb6eca5aa8d86afcb8660468f45a
Sven Meyer <sven.meyer@ns.nl>
2025-06-11 20:36:50 +0200

    Satisfy test: The first square placed will become an X.

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,55 @@
 export type Player = 'x';
 
+export type Cell = (
+  'topleft'
+  | 'top'
+  | 'topright'
+  | 'left'
+  | 'center'
+  | 'right'
+  | 'bottomleft'
+  | 'bottom'
+  | 'bottomright'
+);
+
+type Board = {
+  [k in Cell]: string;
+};
+
 export class TicTacToeGame {
-  private _currentPlayer: Player
+  private _currentPlayer: Player;
+  private _board: Board;
 
   constructor() {
-    this._currentPlayer = 'x'
+    this._currentPlayer = 'x';
+    this._board = {
+      topleft: ' ',
+      top: ' ',
+      topright: ' ',
+      left: ' ',
+      center: ' ',
+      right: ' ',
+      bottomleft: ' ',
+      bottom: ' ',
+      bottomright: ' ',
+    };
   }
 
   get currentPlayer(): Player {
-    return this._currentPlayer
+    return this._currentPlayer;
+  }
+
+  play(cell: Cell) {
+    this._board[cell] = this.currentPlayer;
   }
 
   get board(): string {
     return `
-     | | 
+    ${this._board.topleft}|${this._board.top}|${this._board.topright}
     -+-+-
-     | | 
+    ${this._board.left}|${this._board.center}|${this._board.right}
     -+-+-
-     | | 
-    `
+    ${this._board.bottomleft}|${this._board.bottom}|${this._board.bottomright}
+    `;
   }
 };
